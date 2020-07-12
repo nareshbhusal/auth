a816518d26f008b568ab41eb53d4789eea1d7d6a
nareshbhusal <nareshsharma.abysslurker@gmail.com>
2020-07-12 18:21:10 +0530

    fix /delete user

--- api/src/routes/user/deleteUser.js
+++ api/src/routes/user/deleteUser.js
@@ -5,6 +5,12 @@ module.exports = async (req, res, next) => {
     try {
         const user_id = req.session.user_id;
         await deleteUser(user_id);
+
+        req.session.destroy((err) => {
+            if(err) {
+                return console.log(err);
+            }
+        });
         res.send('User deleted!');
         next();
 
